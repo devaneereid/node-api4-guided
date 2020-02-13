ddffb0604cea12575d1f0fbdd78cf9a1bde2f298
Devanee Reid <devaneereid@gmail.com>
2020-02-13 13:05:55 -0700

    added get request

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ require('dotenv').config();
 
 const server = require('./api/server.js');
 
-const port = process.env.PORT || 4000;
+// server.use(express.json());
+
+server.get('/', (req, res) => {
+  const quotes = [{
+    id: 1,
+    quote: "The way to get started is to quit talking and begin doing. -Walt Disney",
+    secret: process.env.SECRET
+  }];
+    // res.status(200).json(quotes)
+});
+
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
